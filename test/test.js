0d7b55e313c5e8af17e411265edd14659f4905cd
Chris Hartjes <chartjes@littlehart.net>
2012-10-22 22:44:15 -0400

    Added in test for /transactios/current content

--- test/test.js
+++ test/test.js
@@ -6,7 +6,19 @@ describe('Transaction API', function() {
   it('GET /transactions/current should return 200', function(done) {
     request(app)
       .get('/transactions/current')
+      .expect('Content-Type', /json/)
       .expect(200, done);
+  });
+  
+  it('GET /transactions/current should return expected results', function(done) {
+    var fs = require('fs');
+    var currentData = fs.readFile('./current', 'utf8', function(err, data) { 
+      if (err) { throw err; }
+      return data;
+    });
+    request(app)
+      .get('/transactions/current')
+      .expect(currentData, done);
   });
 });
 
